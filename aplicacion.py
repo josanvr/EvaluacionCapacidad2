ed8e2cf7c0d220ad4e3972bd115e77e68a4fa21c
Andres Vera <j.andresvera11@gmail.com>
2022-05-30 14:11:57 -0500

    Diseño del menu

---
+++ aplicacion.py
@@ -0,0 +1,18 @@
+"""
+Created on Mon May 30 14:07:59 2022
+@author: Andres Vera
+"""
+
+import gestion_archivos
+
+def menu():
+    print("******MENU PRINCIPAL*******")
+    print("1. Crear Archivo")
+    print("2. Agregar Contenido")
+    print("3. Mostrar contenido del Archivo")
+    print("4. Eliminar Archivo")
+    print("5. Salir")
+    
+    
+    
+    
